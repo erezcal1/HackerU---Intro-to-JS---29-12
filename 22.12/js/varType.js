c2d1e47dae0e0a1d650df3734c8831317e6e51bd
erez Calderon <erezcal1@gmail.com>
2021-12-22 13:41:00 +0200

    init

---
+++ 22.12/js/varType.js
@@ -0,0 +1,14 @@
+var a = 10; //number
+var b = "20"; //string
+
+var c = a * b; //number
+var d = a + b; //string
+
+var e = null; //null
+var f; //undefined
+var g = true; //boolean
+
+console.log("a is of type: " + typeof a);
+console.log("b is of type: " + typeof b);
+console.log("c = " + c + " and is a type of: " + typeof c);
+console.log("e is a type of: " + typeof e);
